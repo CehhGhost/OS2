44f3af075f62d1c3f3193fd89198ad5824e3ac0b
CehhGhost <71556063+CehhGhost@users.noreply.github.com>
2023-04-22 19:58:03 +0300

    Update main.c

--- FirstProg/C code/main.c
+++ FirstProg/C code/main.c
@@ -57,7 +57,7 @@ void* client(void* arg) {
     c->budget -= room_price;
     printf("Client %d has occupied room %d for %d\n", c->id, room_number, room_price);
     sem_post(sem_mutex);
-    usleep((rand() % 5 + 1) * 200000);
+    usleep((rand() % 5 + 1) * 50000);
     sem_wait(sem_mutex);
     rooms[room_number].client = -1;
     sem_post(sem_mutex);
